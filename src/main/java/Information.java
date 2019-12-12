a0c9e0b66577d92f938067e6e5bd3acbb25d41b4
Alexander Biryulyov <medvedkama@icloud.com>
2019-12-12 16:27:45 +0300

    Add getFields and getMethods

--- src/main/java/Information.java
+++ src/main/java/Information.java
@@ -1,26 +1,47 @@
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Collections;
 
 public class Information {
 
     public String getInfo(Class clazz){
-        String AllInfo = "312";
-        AllInfo = getFields(clazz);
+        String AllInfo = null;
+        getFields(clazz);
+        getMethods(clazz);
         return AllInfo;
 //    getParents(clazz)
     }
 
-    private String getFields(Class clazz){
+    private void getFields(Class clazz){
         String Fields = null;
         Field[] field = clazz.getDeclaredFields();
-        Fields = field.toString();
-        return Fields;
+        int cout = field.length;
+        System.out.println("Fields: \n" + "==========================" + "\t " + cout);
+        for (int i = 0; i <field.length ; i++) {
+            System.out.println(field[i]);
+        }
     }
 
-    private String getParents(Class clazz){
-        String Methods;
-        Methods = "dsada";
-        return Methods;
+    private void getMethods(Class clazz){
+        String Methods = null;
+        Method[] methods = clazz.getDeclaredMethods();
+        int cout = methods.length;
+        System.out.println("\nMethods: \n" + "==========================" + "\t" + cout);
+        for (int i = 0; i <methods.length ; i++) {
+            System.out.println(methods[i]);
+        }
     }
+
+    private void getParent(Class clazz){
+        String Methods = null;
+        Method[] methods = clazz.getDeclaredMethods();
+        int cout = methods.length;
+        System.out.println("\nMethods: \n" + "==========================" + cout);
+        for (int i = 0; i <methods.length ; i++) {
+            System.out.println(methods[i]);
+        }
+    }
+
 
 
 }
