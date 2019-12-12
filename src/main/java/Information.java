e00a27cd32107092bf91e10cfe19cdb36d940561
Alexander Biryulyov <medvedkama@icloud.com>
2019-12-12 17:18:00 +0300

    Delete TestClass. Update Information and Main class.

--- src/main/java/Information.java
+++ src/main/java/Information.java
@@ -4,44 +4,49 @@ import java.util.Collections;
 
 public class Information {
 
-    public String getInfo(Class clazz){
+    public String getInfo(Class clazz) {
         String AllInfo = null;
         getFields(clazz);
         getMethods(clazz);
+        getInterfaces(clazz);
+        getParent(clazz);
         return AllInfo;
-//    getParents(clazz)
     }
 
-    private void getFields(Class clazz){
+    private void getFields(Class clazz) {
         String Fields = null;
         Field[] field = clazz.getDeclaredFields();
         int cout = field.length;
-        System.out.println("Fields: \n" + "==========================" + "\t " + cout);
-        for (int i = 0; i <field.length ; i++) {
+        System.out.println("\nFields: \n" + "==========================" + "\t " + cout);
+        for (int i = 0; i < field.length; i++) {
             System.out.println(field[i]);
         }
     }
 
-    private void getMethods(Class clazz){
+    private void getInterfaces(Class clazz) {
+        String Fields = null;
+        Class[] field = clazz.getInterfaces();
+        int cout = field.length;
+        System.out.println("\nInterfaces: \n" + "==========================" + "\t " + cout);
+        for (int i = 0; i < field.length; i++) {
+            System.out.println(field[i]);
+        }
+    }
+
+    private void getMethods(Class clazz) {
         String Methods = null;
         Method[] methods = clazz.getDeclaredMethods();
         int cout = methods.length;
         System.out.println("\nMethods: \n" + "==========================" + "\t" + cout);
-        for (int i = 0; i <methods.length ; i++) {
+        for (int i = 0; i < methods.length; i++) {
             System.out.println(methods[i]);
         }
     }
 
-    private void getParent(Class clazz){
-        String Methods = null;
-        Method[] methods = clazz.getDeclaredMethods();
-        int cout = methods.length;
-        System.out.println("\nMethods: \n" + "==========================" + cout);
-        for (int i = 0; i <methods.length ; i++) {
-            System.out.println(methods[i]);
+    private void getParent(Class clazz) {
+        if (clazz.getSuperclass() != null) {
+            System.out.println("\nSuperclasses: \n" + "========================== \n" + clazz.getSuperclass().getName());
+            System.out.println("\n" + getInfo(clazz.getSuperclass()));
         }
     }
-
-
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,18 @@
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-
 public class Main {
 
-    private static final String CLASSFORTEST = "java.lang.String";
+    //private static final String CLASSFORTEST = "java.lang.String";
+    private static final String CLASSFORTEST = "java.lang.reflect.Method";
 
     public static void main(String[] args) {
         Information info = new Information();
         try {
             Class clazz = Class.forName(CLASSFORTEST);
             System.out.println(info.getInfo(clazz));
-
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-
     }
 }
