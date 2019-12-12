7f7d346cd55b4e13ec0240508be2c550dea185ae
Alexander Biryulyov <medvedkama@icloud.com>
2019-12-12 14:44:09 +0300

    first commit

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,38 @@
+public class TestClass {
+    private int age;
+    private int number;
+    private String FirstName;
+    private String LastName;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+}
